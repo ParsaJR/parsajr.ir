e381ca20c8ac76658c2bb36487ba348bea455577
ParsaJR <parsajr@proton.me>
2024-10-09 12:17:38 +0330

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -8,16 +8,16 @@
         <p class="dark:text-gray-300 text-gray-700">I try to do it in a list. So, it goes like this:</p>
         <h4 class="font-bold pt-2">Life in a Nutshell</h4>
         <ul class="list-disc list-inside p-3 flex flex-col gap-5">
-            <li>Expert at the basics — like drinking water and food</li>
-            <li>I was born at a very young age, which means I’ve had plenty of time to become a professional at... well, being me!</li>
+            <li>Expert at the basics — like drinking water and food.</li>
+            <li>Trying to learn to play chess, and even more so, trying to stick with it.</li>
             <li>Playing video games, reading about all sort of topics and cooking  — these are some of my favorite ways to spend my free time.</li>
         </ul>
         <h4 class="font-bold pt-2">
             Tech Stuff
         </h4>
         <ul class="list-disc list-inside p-3 flex flex-col gap-5">
-            <li>Languages: familiar with C# (.Net ecosystem) & Javascript (Vue JS ecosystem).</li>
-            <li>Databases: familiar with both SQL (MSSQL) & NoSQL (MongoDB) databases.</li>
+            <li>Languages: Familiar with C# (.Net ecosystem) & Javascript (Vue JS ecosystem).</li>
+            <li>Databases: Familiar with SQL & NoSQL databases.</li>
             <li>DevOps: Familiar with Docker, Linux and CI/CD pipelines for efficient development workflows.</li>
         </ul>
         <br>
