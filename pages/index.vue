349db8dc4e3f6570686e584cbcb2b70ff12fa86f
Parsa Junior <parsajr@proton.me>
2024-12-12 12:03:24 +0330

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
 <template>
     <h2 class="my-3">Hey! 👋</h2>
     <p class="my-2">My name is <b>Parsa</b> and this is my personal blog/website.</p>
-    <p class="my-3">I usually spend my time working with computers, reading various materials and exploring different things in software development.</p>
+    <p class="my-3">I usually spend my time working with computers, exploring different things in software development.</p>
     <p class="">What do i blog about? Anything related to my intrests.</p>
     <MainRecent></MainRecent>
 </template>
