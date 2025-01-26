eb2fef0d497b25888ba94054b1d77d3f9fa0fab4
ParsaJR <parsajr@proton.me>
2025-01-26 18:38:13 +0330

    seo: lang html attribute

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+	  head: {
+		  htmlAttrs: {
+			lang: 'en'
+		  }
+	  }
+  },
   modules: [
     "@nuxt/content",
     "@nuxt/fonts",
