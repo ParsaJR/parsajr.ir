e01b6e283990b06c4b9503140bca84ec16ddad58
ParsaJR <parsajr@proton.me>
2025-04-17 19:29:24 +0330

    Alert component

--- components/content/Alert.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div role="alert" class="rounded border-s-4 border-red-500 bg-red-50 p-4 dark:border-red-600 dark:bg-red-900">
-        <strong class="block font-medium text-red-800 dark:text-red-100"> Alert </strong>
-
-        <p class="mt-2 text-sm text-red-700 dark:text-red-200">
-            <slot></slot>
-        </p>
-    </div>
-</template>
