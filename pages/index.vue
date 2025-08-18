73a16a8fa6526761ae2fc14c17d9afab814ffb55
ParsaJR <parsajr@proton.me>
2025-08-19 00:53:16 +0330

    Intro enhancement

--- pages/index.vue
+++ pages/index.vue
@@ -13,5 +13,6 @@
     <h2 class="my-3">Hey! 👋</h2>
     <p class="my-2">My name is <b>Parsa</b> and this is my personal blog/website.</p>
     <p class="">What do i blog about? Anything related to my intrests.</p>
+    <p class="">By the way i can assure you that everything here is 100% human-made.</p>
     <MainRecent></MainRecent>
 </template>
