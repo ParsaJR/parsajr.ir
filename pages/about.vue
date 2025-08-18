2d351913be2ca33e3323bc0041cb1f4f12c8114b
ParsaJR <parsajr@proton.me>
2025-08-19 00:46:08 +0330

    about

--- pages/about.vue
+++ pages/about.vue
@@ -21,19 +21,29 @@ useHead({
         <p class="dark:text-gray-300 text-gray-700">I try to do it in a list. So, it goes like this:</p>
         <h4 class="font-bold pt-2">Life in a Nutshell</h4>
         <ul class="list-disc list-inside p-3 flex flex-col gap-5">
-            <li>Expert at the basics — like drinking water and food.</li>
-            <li>Playing video games, reading about all sort of topics and cooking  — these are some of my favorite ways to spend my free time.</li>
+            <li>Expert at the basics as a human being — staying hydrated and fed.</li>
+            <li>Playing video games, reading about all sort of topics and cooking — these are some of my favorite ways
+                to spend my free time.</li>
         </ul>
         <h4 class="font-bold pt-2">
             Tech Stuff
         </h4>
         <ul class="list-disc list-inside p-3 flex flex-col gap-5">
-            <li>Languages: Familiar with C# (.Net ecosystem) & Javascript (Vue JS ecosystem).</li>
-            <li>Databases: Familiar with popular SQL & NoSQL databases.</li>
-            <li>DevOps: Familiar with Docker, Linux and CI/CD pipelines for efficient development workflows.</li>
+            <li>
+                Keywords below represent things I'm familiar with and interested in, listed
+                without any particular order:
+                <div class="mt-3 ml-4 flex flex-wrap gap-x-2 justify-center">
+                    <span>Golang,</span>
+                    <span>JavaScript,</span>
+                    <span>Python,</span>
+                    <span>GNU/Linux,</span>
+                    <span>Containerization & Orchestration,</span>
+                    <span>TCP/IP,</span>
+                    <span>Full-Stack Development</span>
+                </div>
+            </li>
         </ul>
         <br>
-        <p>If you’d like to get in touch, connect with me via the links at the bottom of the page. I’d love to
-            hear from you!</p>
+        <p>If you’d like to get in touch, connect with me via the links at the bottom of the page.</p>
     </div>
 </template>
